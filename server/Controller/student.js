42d1e7e7c379f0e723a4d4f98e401c596f86b82b
Satyamrk <satyamrk18@gmail.com>
2025-11-16 16:39:59 +0530

    API modification by adding empty string handling

--- server/Controller/student.js
+++ server/Controller/student.js
@@ -3,62 +3,77 @@ import fs from "fs";
 import Student from "./../Model/Student.js";
 //add student
 const poststudent = async (req, res) => {
-  try
-  {
+  try {
     const {
-    name,
-    father_name,
-    mother_name,
-    address,
-    rollNo,
-    year_of_study,
-    branch,
-    aadhaNo,
-    year,
-  } = req.body;
+      name,
+      father_name,
+      mother_name,
+      address,
+      rollNo,
+      year_of_study,
+      branch,
+      aadhaNo,
+      year,
+    } = req.body;
 
-  const word = branch.split(" ");
-  const branch_ID = (word[0][0] + word[1][0]).toUpperCase();
-  const uniqueNo = `${year}${branch_ID}${aadhaNo
-    .toString()
-    .substring(0, 4)}-${rollNo}`.trim();
+    if (
+      !name ||
+      !father_name ||
+      !mother_name ||
+      !address ||
+      !rollNo ||
+      !year_of_study ||
+      !branch ||
+      !aadhaNo ||
+      !year
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    } else {
+      const word = branch.split(" ");
+      const branch_ID = (word[0][0] + word[1][0]).toUpperCase();
+      const uniqueNo = `${year}${branch_ID}${aadhaNo
+        .toString()
+        .substring(0, 4)}-${rollNo}`.trim();
 
-  //college unique ID generation
-  const college_ID = `${
-    year_of_study == "First year"
-      ? `F${uniqueNo}`
-      : year_of_study == "Second year"
-      ? `S${uniqueNo}`
-      : year_of_study == "Third year"
-      ? `T${uniqueNo}`
-      : year_of_study == "BE"
-      ? `B${uniqueNo}`
-      : ""
-  }`;
-  //slug generation
-  const slug = `${name.toLowerCase().replace(/\s+/g, "-")}-${uniqueNo}`;
-  const studentData = new Student({
-    name,
-    father_name,
-    mother_name,
-    address,
-    rollNo,
-    year_of_study,
-    branch,
-    aadhaNo,
-    year,
+      //college unique ID generation
+      const college_ID = `${
+        year_of_study == "First year"
+          ? `F${uniqueNo}`
+          : year_of_study == "Second year"
+          ? `S${uniqueNo}`
+          : year_of_study == "Third year"
+          ? `T${uniqueNo}`
+          : year_of_study == "BE"
+          ? `B${uniqueNo}`
+          : ""
+      }`;
+      //slug generation
+      const slug = `${name.toLowerCase().replace(/\s+/g, "-")}-${uniqueNo}`;
+      const studentData = new Student({
+        name,
+        father_name,
+        mother_name,
+        address,
+        rollNo,
+        year_of_study,
+        branch,
+        aadhaNo,
+        year,
 
-    college_ID,
-    slug,
-  });
-  const saveStudet = await studentData.save();
-  res.json({
-    success: true,
-    data: saveStudet,
-    message: "student added successfully",
-  });
-  }
-  catch (error) {
+        college_ID,
+        slug,
+      });
+      const saveStudet = await studentData.save();
+      res.json({
+        success: true,
+        data: saveStudet,
+        message: "student added successfully",
+      });
+    }
+  } catch (error) {
     res.status(500).json({
       success: false,
       message: "Internal Server Error",
@@ -182,7 +197,7 @@ const postStudentLogin = async (req, res) => {
     doc.text(`Mother Name: ${studentData.mother_name}`);
     doc.text(`Roll No: ${studentData.rollNo}`);
     doc.text(`Branch: ${studentData.branch}`);
-      doc.text(`Addhar No:: ${studentData.aadhaNo}`);
+    doc.text(`Addhar No:: ${studentData.aadhaNo}`);
     doc.text(`Year of study: ${studentData.year_of_study}`);
     doc.text(`Address : ${studentData.address}`);
     doc.text(`Year: ${studentData.year}`);
